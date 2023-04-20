1516a80d3c3bc2545f1da30ca90154a31bb7f2fa
nbollis <nbollis@comcast.net>
2023-04-20 15:19:41 -0500

    read in msalign files from topFD and flash deconv

--- mzLib/Readers/MsAlign/MsAlign.cs
+++ mzLib/Readers/MsAlign/MsAlign.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using MassSpectrometry;
 using System.IO;
+using Easy.Common.Extensions;
+using MzLibUtil;
 
 namespace Readers
 {
@@ -16,13 +18,14 @@ namespace Readers
         Unknown,
     }
 
-    public class MsAlign : MassSpectrometry.MsDataFile
+    public class MsAlign : MsDataFile
     {
 
         public MsAlignType MsAlignType { get; set; }
 
         public MsAlign(string filePath) : base(filePath)
         {
+            Header = new();
             if (filePath.ToLower().Contains("ms1.msalign"))
             {
                 MsAlignType = MsAlignType.Ms1;
@@ -54,16 +57,18 @@ namespace Readers
             using (StreamReader sr = new StreamReader(FilePath))
             {
                 string? line;
+                List<string> linesToProcess = new();
                 while ((line = sr.ReadLine()) is not null)
                 {
-                    List<string> linesToProcess = new();
+                    if (line.Contains("BEGIN IONS"))
+                        headerProgress = ReadingProgress.Finished;
 
                     // get header
                     if (headerProgress != ReadingProgress.Finished)
                     {
-                        if (headerProgress == ReadingProgress.NotFound && line.Contains("#####Parameters#####"))
+                        if (headerProgress == ReadingProgress.NotFound && line.Contains("##### Parameters #####"))
                             headerProgress = ReadingProgress.Found;
-                        else if (headerProgress == ReadingProgress.Found && line.Contains("#####Parameters#####"))
+                        else if (headerProgress == ReadingProgress.Found && line.Contains("##### Parameters #####"))
                         {
                             headerProgress = ReadingProgress.Finished;
                             ParseHeaderLines(linesToProcess); // TODO: this method
@@ -75,20 +80,22 @@ namespace Readers
                             continue;
                         }
                     }
-
-                    // each entry after header
-                    if (entryProgress == ReadingProgress.NotFound && line.Contains("BEGIN IONS"))
-                        entryProgress = ReadingProgress.Found;
-                    else if (entryProgress == ReadingProgress.Found && line.Contains("END IONS"))
-                    {
-                        entryProgress = ReadingProgress.NotFound;
-                        scans.Add(ParseEntryLines(linesToProcess)); // TODO: this method
-                        linesToProcess.Clear();
-                    }
                     else
                     {
-                        linesToProcess.Add(line);
-                        continue;
+                        // each entry after header
+                        if (entryProgress == ReadingProgress.NotFound && line.Contains("BEGIN IONS"))
+                            entryProgress = ReadingProgress.Found;
+                        else if (entryProgress == ReadingProgress.Found && line.Contains("END IONS"))
+                        {
+                            entryProgress = ReadingProgress.NotFound;
+                            scans.Add(ParseEntryLines(linesToProcess)); // TODO: this method
+                            linesToProcess.Clear();
+                        }
+                        else
+                        {
+                            linesToProcess.Add(line);
+                            continue;
+                        }
                     }
                 }
             }
@@ -118,15 +125,132 @@ namespace Readers
         }
 
 
-
+        private Dictionary<string, string> Header { get; set; }
         private void ParseHeaderLines(List<string> headerLines)
         {
-            throw new NotImplementedException();
+            foreach (var line in headerLines.Where(p => p.Contains('\t')))
+            {
+                var splits = line.Split('\t');
+                Header.Add(splits[0].Trim(), splits[1].Replace("m/z","").Trim());
+            }
         }
 
         private MsDataScan ParseEntryLines(List<string> entryLines)
         {
-            throw new NotImplementedException();
+            // all
+            int id;
+            int fractionId;
+            string fileName;
+            int oneBasedScanNumber = 0;
+            double retentionTime = 0;
+            int msnOrder = 0;
+
+            // ms2
+            DissociationType? dissociationType = null;
+            int? precursorScanId = null;
+            int? oneBasedPrecursorScanNumber = null;
+            double? precursorMz = null;
+            int? precursorCharge = null;
+            double? precursorMass = null;
+            double? precursorIntensity = null;
+
+            foreach (var headerLine in entryLines.Where(p => p.Contains('=')))
+            {
+                var splits = headerLine.Split('=');
+                switch (splits[0])
+                {
+                    case "ID":
+                        id = int.Parse(splits[1]);
+                        break;
+                    case "FRACTION_ID":
+                        fractionId = int.Parse(splits[1]);
+                        break;
+                    case "FILE_NAME":
+                        fileName = splits[1];
+                        break;
+                    case "SCANS":
+                        oneBasedScanNumber = int.Parse(splits[1]);
+                        break;
+                    case "RETENTION_TIME":
+                        retentionTime = double.Parse(splits[1]);
+                        break;
+                    case "LEVEL":
+                        msnOrder = int.Parse(splits[1]);
+                        break;
+                    case "ACTIVATION":
+                        dissociationType = splits[1] switch
+                        {
+                            "HCD" => DissociationType.HCD,
+                            "FILE" => DissociationType.Autodetect,
+                            "CID" => DissociationType.CID,
+                            "ETD" => DissociationType.ETD,
+                            "BIRD" => DissociationType.BIRD,
+                            "ECD" => DissociationType.ECD,
+                            "ISCID" => DissociationType.ISCID,
+                            "LOWCID" => DissociationType.LowCID,
+                            "EThcD" => DissociationType.EThcD,
+                            "IRMPD" => DissociationType.IRMPD,
+                            "MPD" => DissociationType.MPD,
+                            "NETD" => DissociationType.NETD,
+                            "PD" => DissociationType.PD,
+                            "PQD" => DissociationType.PQD,
+                            "PSD" => DissociationType.PSD,
+                            "SID" => DissociationType.SID,
+                            "SORI" => DissociationType.SORI,
+                            "UVPD" => DissociationType.UVPD,
+                            _ => throw new ArgumentOutOfRangeException()
+                        };
+                        break;
+                    case "MS_ONE_ID":
+                        precursorScanId = int.Parse(splits[1]);
+                        break;
+                    case "MS_ONE_SCAN":
+                        oneBasedPrecursorScanNumber = int.Parse(splits[1]);
+                        break;
+                    case "PRECURSOR_MZ":
+                        precursorMz = double.Parse(splits[1]);
+                        break;
+                    case "PRECURSOR_CHARGE":
+                        precursorCharge = int.Parse(splits[1]);
+                        break;
+                    case "PRECURSOR_MASS":
+                        precursorMass = double.Parse(splits[1]);
+                        break;
+                    case "PRECURSOR_INTENSITY":
+                        precursorIntensity = double.Parse(splits[1]);
+                        break;
+                }
+            }
+
+            var peakLines = entryLines.Where(p => p.Contains('\t')).ToArray();
+            var mzs = new double[peakLines.Length];
+            var intensities = new double[peakLines.Length];
+            var charges = new int[peakLines.Length];
+
+            for (int i = 0; i < peakLines.Length; i++)
+            {
+                var splits = peakLines[i].Split('\t');
+
+                charges[i] = int.Parse(splits[2]);
+                mzs[i] = (double.Parse(splits[0]) + charges[i]) / charges[i];
+                intensities[i] = double.Parse(splits[1]);
+            }
+
+            var spectrum = new MzSpectrum(mzs, intensities, true);
+
+
+            // TODO replace the below with info from the header
+            // centroided, precursor window, mzrange
+            var t = new MsDataScan(spectrum, oneBasedScanNumber, msnOrder, true, Polarity.Positive, retentionTime,
+                mzs.Any() ? new MzRange(mzs.Min(), mzs.Max()) : new MzRange(0, 2000), null, MZAnalyzerType.Orbitrap,
+                intensities.Sum(), null, null, null, precursorMz, 
+                precursorCharge, precursorIntensity, precursorMz,
+                Header.TryGetValue("Precursor window size:", out string value) ? double.Parse(value) : 3,
+                dissociationType, oneBasedPrecursorScanNumber, null, null);
+
+            return t;
         }
+
+
     }
 }
--- mzLib/Test/FileReadingTests/TestMsAlign.cs
+++ mzLib/Test/FileReadingTests/TestMsAlign.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using MassSpectrometry;
@@ -14,13 +16,103 @@ namespace Test.FileReadingTests
     public class TestMsAlign
     {
 
+        private const string lvs22averaged =
+            @"B:\Users\Nic\ScanAveraging\AveragedDataBulkJurkat\TopFD\CalibAveraged\id_02-17-20_jurkat_td_rep2_fract2-calib-averaged-centroided_file\id_02-17-20_jurkat_td_rep2_fract2-calib-averaged-centroided_ms1.msalign";
+        private const string lvs22calib =
+            @"B:\Users\Nic\ScanAveraging\AveragedDataBulkJurkat\TopFD\Calib\id_02-17-20_jurkat_td_rep2_fract2-calib-centroided_file\id_02-17-20_jurkat_td_rep2_fract2-calib-centroided_ms1.msalign";
+
+
+
+        private const string FlashDeconAveragedDirectory =
+            @"B:\Users\Nic\ScanAveraging\AveragedDataBulkJurkat\FLASHDeconNoCentroid\CalibAveraged";
+        private const string FlashDeconCalibDirectory =
+            @"B:\Users\Nic\ScanAveraging\AveragedDataBulkJurkat\FLASHDeconNoCentroid\Calib";
+
+        private const string TopFDAveragedDirectory =
+            @"B:\Users\Nic\ScanAveraging\AveragedDataBulkJurkat\TopFD\CalibAveraged";
+        private const string TopFDCalibDirectory =
+            @"B:\Users\Nic\ScanAveraging\AveragedDataBulkJurkat\TopFD\Calib";
+
+
         [Test]
-        [TestCase("@DataFiles/LVS_jurkat_td_rep2_fract2-calib-averaged-centroided_ms1.msalign")]
+        [TestCase(@"DataFiles/LVS_jurkat_td_rep2_fract2-calib-averaged-centroided_ms1.msalign")]
         public void FirstTestMsAlign(string filePath)
         {
             string spectraPath = Path.Combine(TestContext.CurrentContext.TestDirectory, filePath);
             MsDataFile datafile = MsDataFileReader.GetDataFile(spectraPath);
             var t = datafile.LoadAllStaticData();
+        }
+
+        public enum DeconType
+        {
+            FLASHDecon,
+            TopFD,
+        }
+
+        [Test]
+        [TestCase(FlashDeconCalibDirectory, FlashDeconAveragedDirectory, DeconType.FLASHDecon)]
+        [TestCase(TopFDCalibDirectory, TopFDAveragedDirectory, DeconType.TopFD)]
+        public void CompareFlashDeconvMs1Align(string calibDirectory, string averagedDirectory, DeconType type)
+        {
+            var calibFiles = GetMs1AlignFiles(calibDirectory, type);
+            var averagedFiles = GetMs1AlignFiles(averagedDirectory, type);
+
+            List<MsAlignResults> results = new();
+
+            for (var i = 0; i < calibFiles.Length; i++)
+            {
+                var calibFile = calibFiles[i];
+                var averagedFile = averagedFiles[i];
+
+                var withoutExtension = Path.GetFileNameWithoutExtension(averagedFile);
+                var longName = withoutExtension.Substring(withoutExtension.IndexOf('_') + 1,
+                    withoutExtension.Length - withoutExtension.IndexOf('_') - 1);
+                var fileName = longName.Remove(longName.IndexOf("-calib"));
+                if (type == DeconType.TopFD)
+                    fileName = fileName.Substring(fileName.IndexOf('_') + 1, fileName.Length - fileName.IndexOf('_') - 1);
+
+                var calibPeakCount = MsDataFileReader.GetDataFile(calibFile)
+                    .GetMS1Scans()
+                    .Sum(p => p.MassSpectrum.XArray.Length);
+                var averagedPeakCount = MsDataFileReader.GetDataFile(averagedFile)
+                    .GetMS1Scans()
+                    .Sum(p => p.MassSpectrum.XArray.Length);
+
+                results.Add(new MsAlignResults(type, fileName, calibPeakCount, averagedPeakCount));
+            }
+
+            string outDirectory =
+                @"C:\Users\Nic\OneDrive - UW-Madison\AUSTIN V CARR - AUSTIN V CARR's files\SpectralAveragingPaper\ResultsData\Deconvolution";
+            string outPath = Path.Combine(outDirectory, $"{type}_ms1Align_PeakCounting.csv");
+            using (var sw = new StreamWriter(outPath))
+            {
+                sw.WriteLine("Software,File,Calib,CalibAveraged");
+                foreach (var result in results)
+                {
+                    sw.WriteLine($"{result.Type},{result.FileName},{result.Calib},{result.Averaged}");
+                }
+            }
+        }
+
+        public record struct MsAlignResults(DeconType Type, string FileName, double Calib, double Averaged);
+
+
+        private string[] GetMs1AlignFiles(string directoryPath, DeconType type)
+        {
+            return type switch
+            {
+                DeconType.FLASHDecon => 
+                    Directory.GetFiles(directoryPath)
+                        .Where(p => p.EndsWith("ms1.msalign"))
+                        .OrderBy(p => p)
+                        .ToArray(),
+                DeconType.TopFD => 
+                    Directory.GetDirectories(directoryPath)
+                        .Where(p => p.EndsWith("_file"))
+                        .SelectMany(p => Directory.GetFiles(p).Where(m => m.EndsWith("ms1.msalign")))
+                        .OrderBy(p => p)
+                        .ToArray()
+            };
         }
     }
 }
