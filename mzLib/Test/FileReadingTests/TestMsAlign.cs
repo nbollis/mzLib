1a12f6eae2803291538c57eef3ad60075664ec4f
nbollis <nbollis@comcast.net>
2023-04-20 11:59:22 -0500

    made msalign test

---
+++ mzLib/Test/FileReadingTests/TestMsAlign.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassSpectrometry;
+using NUnit.Framework;
+using Readers;
+
+namespace Test.FileReadingTests
+{
+    [TestFixture]
+    public class TestMsAlign
+    {
+
+        [Test]
+        [TestCase("@DataFiles/LVS_jurkat_td_rep2_fract2-calib-averaged-centroided_ms1.msalign")]
+        public void FirstTestMsAlign(string filePath)
+        {
+            string spectraPath = Path.Combine(TestContext.CurrentContext.TestDirectory, filePath);
+            MsDataFile datafile = MsDataFileReader.GetDataFile(spectraPath);
+        }
+    }
+}
